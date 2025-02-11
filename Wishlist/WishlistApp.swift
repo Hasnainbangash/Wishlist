fee07bfbe7f5a0cc90893dd6548ab9110896199c
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-11 12:58:51 +0500

    Swift data model is set up in the wishlist app

--- Wishlist/WishlistApp.swift
+++ Wishlist/WishlistApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct WishlistApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: Wish.self) // This model container creates and sets up a storage for the wish object. Also it enabales access to the storage inside the window group which is the entire app. And finally, this container reads the Swift model to understand how the data objects should be saved.
         }
     }
 }
