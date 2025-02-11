0c4901c09d22e4a05c3db5ee356ff9d2108977d7
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-11 11:47:08 +0500

    Initial Commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Elexoft on 11/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
